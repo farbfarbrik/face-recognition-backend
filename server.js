83dab1dcb49dc67c795546a409d078b6246e2ac0
Sascha Dörnath <sascha.doernath@gmx.de>
2020-03-09 21:56:13 +0100

    Connect database to backend

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const cors = require('cors');
+const knex = require('knex');
 
+const database = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: 'postgres',
+		database: 'facerecognition'
+	}
+});
 
 const app = express();
 
@@ -10,98 +20,91 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'john',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'sally',
-			email: 'sally@gmail.com',
-			password: 'bananas',
-			entries: 0,
-			joined: new Date()
-		},
-	],
-	login: [
-		{
-			id: '987',
-			hash: '',
-			email: 'john@gmail.com'
-		}
-	]
-}
-
 app.get('/', (req, res) => {
 	res.json(database.users);
 })
 
 app.post('/signin', (req, res) => {
-	const hash = '$2b$10$YripV.m.oz5aG5Z/uU/f0uyHbNPfkChnweL9ByT2.CEu4oIGF2qvy';
-	bcrypt.compare("apples", hash, (err, res) => {
-		console.log('first guess', res)
-	});
-	bcrypt.compare("veggies", hash, (err, res) => {
-		console.log('second guess', res)
-	});
-	if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-		res.json(database.users[0]);
-	} else {
-		res.status(400).json('error logging in');
-	}
+	database.select('email', 'hash').from('login')
+		.where('email', '=', req.body.email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				return database.select('*').from('users')
+					.where('email', '=', req.body.email)
+					.then(user => {
+						res.json(user[0])
+					})
+					.catch(err => {
+						res.status(400).json('unable to get user')
+					})
+			} else {
+				res.status(400).json('wrong credentials')
+			}
+		})
+		.catch(err => {
+			res.status(400).json('wrong credentials')
+		})
 })
 
 app.post('/register', (req, res) => {
 	const { email, password, name } = req.body;
-	bcrypt.hash(password, saltRounds, (err, hash) => {
-		console.log(hash);
-	});
-	database.users.push({
-		id: '125',
-		name: name,
-		email: email,
-		password: password,
-		entries: 0,
-		joined: new Date()
+	const hash = bcrypt.hashSync(password, saltRounds);
+	database.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginemail => {
+			return database('users')
+				.returning('*')
+				.insert({
+					email: loginemail[0],
+					name: name,
+					joined: new Date()
+				})
+				.then(user => {
+					res.json(user[0]);
+				})
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
 	})
-	res.json(database.users[database.users.length-1]);
+	.catch(err => {
+		res.status(400).json('unable to register');		
+	})
 })
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		}
-	})
-	if (!found) {
-		res.status(404).json('not found');
-	}
+	database.select('*').from('users').where({id})
+		.then(user => {
+			if (user.length) {
+				res.json(user[0]);
+			} else {
+				res.status(400).json('error, not found')
+			}
+		})
+		.catch(err => {
+			res.status(400).json('error getting user')
+		})
 })
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	})
-	if (!found) {
-		res.status(404).json('not found');
-	}
+	database('users').where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => {
+			res.json(entries[0])
+		})
+		.catch(err => {
+			res.status(400).json('unable to get entries')
+		})
 })
 
-app.listen(1234, () => {
-	console.log("app is running on port 1234");
+app.listen(3001, () => {
+	console.log("app is running on port 3001");
 })
