62f28754116d58f195174f24354eb70f76deb064
Sascha Dörnath <sascha.doernath@gmx.de>
2020-04-30 13:35:36 +0200

    fix connection string

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const imageurl = require('./controllers/image');
 const database = knex({
 	client: 'pg',
 	connection: {
-		host: proces.env.DATABASE_URL,
+		connectionString: proces.env.DATABASE_URL,
 		ssl: true,
 	}
 });
